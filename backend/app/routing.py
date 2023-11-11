12cf08fa03ad996bdc9648d582495fa06a2fd3b5
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2023-11-11 18:06:26 +0200

    bump reactapp

--- backend/app/routing.py
+++ backend/app/routing.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from app.consumers import AiChatRoomConsumer, MeasurementAlertRoomConsumer
+from app.consumers import AiChatRoomConsumer, MeasurementAlertRoomConsumer, MeasurementsRoomConsumer
 
 websocket_urlpatterns = [
     re_path(r'^ws/ai-chat/(?P<room_name>[^/]+)/$', AiChatRoomConsumer.as_asgi()),
     re_path(r'^ws/measurement-alerts/(?P<room_name>[^/]+)/$', MeasurementAlertRoomConsumer.as_asgi()),
+    re_path(r'^ws/measurements/(?P<room_name>[^/]+)/$', MeasurementsRoomConsumer.as_asgi()),
 ]
