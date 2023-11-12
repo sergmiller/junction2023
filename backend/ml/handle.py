cee9c1b98f2b459f4b3bb2f2c59473c3cb4e1933
Serg Miller <sergey.miller@phystech.edu>
2023-11-12 13:42:59 +0200

    add print request

--- backend/ml/handle.py
+++ backend/ml/handle.py
@@ -3,5 +3,6 @@ from .cascade_retriever import CascadeRetriever
 
 
 def ask(request: str):
+    print("REQUEST: " + request)
     model = CascadeRetriever.get_or_create()
     return model.process(request)
